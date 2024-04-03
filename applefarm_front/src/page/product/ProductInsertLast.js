70d327d6e390e629aaabdf4518624f7bd73180f1
qkrqkrtm <mp2221mp2221@gmail.com>
2024-04-03 18:28:12 +0900

    등록 거의 마무리

--- applefarm_front/src/page/product/ProductInsertLast.js
+++ applefarm_front/src/page/product/ProductInsertLast.js
@@ -1,4 +1,50 @@
+import { useState } from "react";
+import ProductInsertLastFrm from "./ProductInsertLastFrm";
+
 const ProductInsertLast = (props) => {
+  //데이터 전송용
+  const [title, setTitle] = useState(); //제목
+  const [content, setContent] = useState(); //내용
+  const [price, setPrice] = useState(); //가격
+  const [file, setFile] = useState([]); //이미지
+  const [thumbnail, setThumbnail] = useState(); //대표이미지
+
+  //화면 출력용
+  const [fileList, setFileList] = useState([]); //이미지 미리보기
+  const [image, setImage] = useState(null); //대표이미지 미리보기
+
+  const writeBtn = () => {
+    console.log("제목: ", title); //필수
+    console.log("내용: ", content);
+    console.log("가격: ", price);
+    console.log("파일: ",file);
+    console.log("대표이미지: ", thumbnail);
+    console.log("등록 완료");
+  };
+
+  return (
+    <div className="insert-last-write-wrap">
+      <div className="insert-last-frm-title">상세 입력 페이지</div>
+      <ProductInsertLastFrm
+        title={title}
+        content={content}
+        price={price}
+        file={file}
+        thumbnail={thumbnail}
+        fileList={fileList}
+        image={image}
+        setTitle={setTitle}
+        setContent={setContent}
+        setPrice={setPrice}
+        setFile={setFile}
+        setThumbnail={setThumbnail}
+        setFileList={setFileList}
+        setImage={setImage}
+        writeBtn={writeBtn}
+      />
+    </div>
+  );
+
   return;
 };
 
