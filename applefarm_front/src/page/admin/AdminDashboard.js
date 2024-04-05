a7109bb7f0f2ca58bd9cc8eb9e0e66f774422581
luciankim <devlucian7@gmail.com>
2024-04-05 20:57:35 +0900

    fix charts

--- applefarm_front/src/page/admin/AdminDashboard.js
+++ applefarm_front/src/page/admin/AdminDashboard.js
@@ -181,7 +181,7 @@ const AdminDashboard = () => {
     series: [
       {
         name: "회원수",
-        type: "column",
+        type: "area",
         data: [23, 10, 10, 20, 10, 22, 17, 10, 22, 22, 12, 16],
         // color: "var(--main_01)",
       },
@@ -202,6 +202,7 @@ const AdminDashboard = () => {
         color: "#263238",
       },
     },
+    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
   });
 
   return (
