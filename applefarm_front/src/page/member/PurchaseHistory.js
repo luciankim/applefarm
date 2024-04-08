ce0eeca98e2538acb068134a4f6dceaa70de0b0b
dltkfkd92 <lsr992@naver.com>
2024-04-09 00:36:30 +0900

    거래내역 기간설정

--- applefarm_front/src/page/member/PurchaseHistory.js
+++ applefarm_front/src/page/member/PurchaseHistory.js
@@ -1,13 +1,23 @@
 import { useState } from "react";
 import Tab from "./Tab";
-
+import dayjs, { Dayjs } from "dayjs";
 const PurchaseHistory = () => {
   const [currentTab, setCurrentTab] = useState(0);
+  const [reqPage, setReqPage] = useState(1);
   const [tabMenu, setTabMenu] = useState([
     { name: "구매입찰", content: <PurchaseBid /> },
     { name: "진행중", content: <PurchaseOngoing /> },
     { name: "종료", content: <PurchaseEnd /> },
   ]);
+
+  const [tradeList, setTradeList] = useState([]);
+
+  //처음 기본값 세팅 => startDate:2개월전 / endDate:오늘 / 최근 2개월 조회 활성화
+  const [startDate, setStartDate] = useState(dayjs().subtract(2, "month"));
+  const [endDate, setEndDate] = useState(dayjs());
+  const filterStartDate = startDate.format("YYYY/MM/DD");
+  const filterEndDate = endDate.format("YYYY/MM/DD");
+  const [activeButton, setActiveButton] = useState("twoMonth");
 
   return (
     <div className="mypage-current-wrap">
@@ -18,6 +28,12 @@ const PurchaseHistory = () => {
           setTabMenu={setTabMenu}
           currentTab={currentTab}
           setCurrentTab={setCurrentTab}
+          startDate={startDate}
+          setStartDate={setStartDate}
+          endDate={endDate}
+          setEndDate={setEndDate}
+          activeButton={activeButton}
+          setActiveButton={setActiveButton}
         />
       </div>
     </div>
@@ -32,4 +48,5 @@ const PurchaseOngoing = () => {
 const PurchaseEnd = () => {
   return <div>종료</div>;
 };
+
 export default PurchaseHistory;
