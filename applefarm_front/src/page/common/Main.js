819117a008b5323ef89cdf87250a5bc664c43d30
L1E1e <99781037+L1E1e@users.noreply.github.com>
2024-04-16 21:07:01 +0900

    메인 시작

--- applefarm_front/src/page/common/Main.js
+++ applefarm_front/src/page/common/Main.js
@@ -2,6 +2,8 @@ const Main = () => {
   return (
     <>
       <div className="main-wrap">
+        <img src="../image/wwdc2.jpg" />
+
         <img src="../image/iphoneMain.jpg" />
       </div>
     </>
